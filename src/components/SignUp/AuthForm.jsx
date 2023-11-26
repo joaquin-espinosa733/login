ab361bf4d83c139f34f1bd460a70a1307781f090
joaquin-espinosa733 <juaco.enero9@gmail.com>
2023-11-26 02:02:54 -0300

    transition

--- src/components/SignUp/AuthForm.jsx
+++ src/components/SignUp/AuthForm.jsx
@@ -2,6 +2,7 @@
 import { useState } from 'react';
 import styles from './AuthForm.module.css'; // Importa tu archivo CSS module
 import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
+import Slider from './Slider';
 
 const AuthForm = () => {
   const [activeForm, setActiveForm] = useState('signin');
@@ -11,49 +12,58 @@ const AuthForm = () => {
   };
 
   return (
-    <div className={styles.authContainer}>
-      <div className={styles.containerButtons}>
-        <button
-          className={`${styles.buttons} ${activeForm === 'signin' ? styles.activeButton : ''}`}
-          onClick={() => handleFormChange('signin')}
-        >
-          Sign In
-        </button>
-        <button
-          className={`${styles.buttons2} ${activeForm === 'signup' ? styles.activeButton : ''}`}
-          onClick={() => handleFormChange('signup')}
-        >
-          Sign Up
-        </button>
+    <div className=' container'>
+      <div className=' row d-flex justify-content-center align-items-center'>
+        <div className='col-6 p-0 m-0'>
+          <Slider />
+        </div>
+        <div className=' col-6 p-0 m-0'>
+          <div className={styles.authContainer}>
+            <div className={styles.containerButtons}>
+              <button
+                className={`${styles.buttons} ${activeForm === 'signin' ? styles.activeButton : ''}`}
+                onClick={() => handleFormChange('signin')}
+              >
+                Sign In
+              </button>
+              <button
+                className={`${styles.buttons2} ${activeForm === 'signup' ? styles.activeButton : ''}`}
+                onClick={() => handleFormChange('signup')}
+              >
+                Sign Up
+              </button>
+            </div>
+
+            {/* Formulario de Sign In */}
+            <form
+              className={`container ${styles.authForm} ${activeForm === 'signin' ? styles.active : styles.hidden}`}
+            >
+              {/* Agrega aquí los campos para el inicio de sesión */}
+              <span>Usuario</span>
+              <input type="text" placeholder="Usuario" />
+              <span>Contraseña</span>
+              <input type="password" placeholder="Contraseña" />
+              <br />
+              <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
+            </form>
+
+            {/* Formulario de Sign Up */}
+            <form
+              className={`${styles.authForm} ${activeForm === 'signup' ? styles.active : styles.hidden}`}
+            >
+              {/* Agrega aquí los campos para el registro */}
+              <span>Nombre de Usuario</span>
+              <input type="text" placeholder="Nombre de usuario" /><br />
+              <span>Correo Electronico</span>
+              <input type="email" placeholder="Correo electrónico" /><br />
+              <span>Contraseña</span>
+              <input type="password" placeholder="Contraseña" />
+              <br />
+              <button type="submit" className="btn btn-primary">Registrarse</button>
+            </form>
+          </div>
+        </div>
       </div>
-
-      {/* Formulario de Sign In */}
-      <form
-        className={`container ${styles.authForm} ${activeForm === 'signin' ? styles.active : styles.hidden}`}
-      >
-        {/* Agrega aquí los campos para el inicio de sesión */}
-        <span>Usuario</span>
-        <input type="text" placeholder="Usuario" />
-        <span>Contraseña</span>
-        <input type="password" placeholder="Contraseña" />
-        <br />
-        <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
-      </form>
-
-      {/* Formulario de Sign Up */}
-      <form
-        className={`${styles.authForm} ${activeForm === 'signup' ? styles.active : styles.hidden}`}
-      >
-        {/* Agrega aquí los campos para el registro */}
-        <span>Nombre de Usuario</span>
-        <input type="text" placeholder="Nombre de usuario" /><br />
-        <span>Correo Electronico</span>
-        <input type="email" placeholder="Correo electrónico" /><br />
-        <span>Contraseña</span>
-        <input type="password" placeholder="Contraseña" />
-        <br />
-        <button type="submit" className="btn btn-primary">Registrarse</button>
-      </form>
     </div>
   );
 };
---
+++ src/components/SignUp/Slider.jsx
@@ -0,0 +1,27 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/scrollbar';
+import aver from "../../assets/images/aver.webp"
+
+function Slider() {
+    return (
+        <div>
+            <Swiper
+                modules={[Navigation, Pagination, Scrollbar, A11y]}
+                spaceBetween={50}
+                slidesPerView={1}
+                navigation
+                pagination={{ clickable: true }}
+                scrollbar={{ draggable: true }}
+            >
+                <SwiperSlide><img src={aver} alt="" width={"100%"} height={"610px"}/></SwiperSlide>
+                <SwiperSlide><img src={aver} alt="" width={"100%"} height={"610px"}/></SwiperSlide>
+                <SwiperSlide><img src={aver} alt="" width={"100%"} height={"610px"}/></SwiperSlide>
+                <SwiperSlide><img src={aver} alt="" width={"100%"} height={"610px"}/></SwiperSlide>
+            </Swiper></div>
+    )
+}
+
+export default Slider
