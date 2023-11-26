853f9b827a6f979385c9c5c88c6b3bd91c304e4a
joaquin-espinosa733 <juaco.enero9@gmail.com>
2023-11-26 00:56:41 -0300

    register

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import AuthForm from './components/SignUp/AuthForm';
+function App() {
+
+  return (
+    <div>
+      <AuthForm />
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/SignUp/AuthForm.jsx
@@ -0,0 +1,61 @@
+// AuthForm.js
+import { useState } from 'react';
+import styles from './AuthForm.module.css'; // Importa tu archivo CSS module
+import 'bootstrap/dist/css/bootstrap.min.css'; // Importa los estilos de Bootstrap
+
+const AuthForm = () => {
+  const [activeForm, setActiveForm] = useState('signin');
+
+  const handleFormChange = (formType) => {
+    setActiveForm(formType);
+  };
+
+  return (
+    <div className={styles.authContainer}>
+      <div className={styles.containerButtons}>
+        <button
+          className={`${styles.buttons} ${activeForm === 'signin' ? styles.activeButton : ''}`}
+          onClick={() => handleFormChange('signin')}
+        >
+          Sign In
+        </button>
+        <button
+          className={`${styles.buttons2} ${activeForm === 'signup' ? styles.activeButton : ''}`}
+          onClick={() => handleFormChange('signup')}
+        >
+          Sign Up
+        </button>
+      </div>
+
+      {/* Formulario de Sign In */}
+      <form
+        className={`container ${styles.authForm} ${activeForm === 'signin' ? styles.active : styles.hidden}`}
+      >
+        {/* Agrega aquí los campos para el inicio de sesión */}
+        <span>Usuario</span>
+        <input type="text" placeholder="Usuario" />
+        <span>Contraseña</span>
+        <input type="password" placeholder="Contraseña" />
+        <br />
+        <button type="submit" className="btn btn-primary">Iniciar Sesión</button>
+      </form>
+
+      {/* Formulario de Sign Up */}
+      <form
+        className={`${styles.authForm} ${activeForm === 'signup' ? styles.active : styles.hidden}`}
+      >
+        {/* Agrega aquí los campos para el registro */}
+        <span>Nombre de Usuario</span>
+        <input type="text" placeholder="Nombre de usuario" /><br />
+        <span>Correo Electronico</span>
+        <input type="email" placeholder="Correo electrónico" /><br />
+        <span>Contraseña</span>
+        <input type="password" placeholder="Contraseña" />
+        <br />
+        <button type="submit" className="btn btn-primary">Registrarse</button>
+      </form>
+    </div>
+  );
+};
+
+export default AuthForm;
